9eb3e7654db009fb2dcf5eefcf256eefc30cde6a
ViPhong <viphongly2804@gmail.com>
2023-11-27 14:52:46 +0700

    'done'

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
+import Separator from "./components/Separator";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={[
+            { key: "Android" },
+            { key: "iOS" },
+            { key: "Java" },
+            { key: "Swift" },
+            { key: "Php" },
+            { key: "Hadoop" },
+            { key: "Sap" },
+            { key: "Python" },
+            { key: "Ajax" },
+            { key: "C++" },
+            { key: "Ruby" },
+            { key: "Rails" },
+            { key: ".Net" },
+            { key: "Perl" },
+          ]}
+          renderItem={({ item }) => {
+            return <Text>{item.key}</Text>;
+          }}
+          // ItemSeparatorComponent={Separator}
+          keyExtractor={(item) => item.key}
+        />
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
---
+++ components/Separator.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Separator = () => {
+  return (
+    <View
+      style={{
+        height: 1,
+        width: "100%",
+        backgroundColor: "#000",
+      }}
+    />
+  );
+};
+
+export default Separator;
+
+const styles = StyleSheet.create({});
